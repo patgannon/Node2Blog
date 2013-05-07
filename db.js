63de54a49dd0f1e52fa04ddd393a90b6f38537ca
patgannon <PatrickJosephGannon@yahoo.com>
2013-05-06 22:56:49 -0700

    Use DB called patrickgannon

--- db.js
+++ db.js
@@ -6,7 +6,7 @@ var Schema = mongoose.Schema
 
 //process.env.MONGOHQ_URL is for deploying on heroku
 //node2blog can be chagens to whatever you want your local database to be called i.e. 'my database'
-var db_url = process.env.MONGOHQ_URL || "mongodb://localhost:27017/your_database_name", 
+var db_url = process.env.MONGOHQ_URL || "mongodb://localhost:27017/patrickgannon", 
     db = mongoose.connect(db_url);
 
 //The MongoDB Schema for your posts
